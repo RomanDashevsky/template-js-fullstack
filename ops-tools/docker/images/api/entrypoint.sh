f31d37b5e381c4e31861b4998a723b6735298f52
Roman Dashevsky <rdashevsky@lineate.com>
2019-02-01 22:29:34 +0600

    update api image

--- ops-tools/docker/images/api/entrypoint.sh
+++ ops-tools/docker/images/api/entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-echo ***** Installed npm dependencies *****
-npm i
-
 echo ***** Start app  *****
 
 if [ $DEV_MOD == "YES" ]
 then
   echo ***** In dev mod  *****
-  npm run dev
+  adonis serve --dev --debug=0.0.0.0:9229
 else
   echo ***** In prod mod  *****
   npm run start
